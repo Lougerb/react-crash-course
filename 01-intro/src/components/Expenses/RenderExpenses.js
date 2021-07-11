0ae20c7475948cf315f6f604910cc99643944a2f
LougerB <glouien@gmail.com>
2021-07-11 18:00:13 +0800

    added filter

--- 01-intro/src/components/Expenses/RenderExpenses.js
+++ 01-intro/src/components/Expenses/RenderExpenses.js
@@ -1,6 +1,8 @@
+import React, { useState } from "react";
 import ExpenseItem from "../Expenses/ExpenseItem";
 import ExpensesData from "../Expenses/ExpensesData";
 import NewExpense from "../NewExpense/NewExpense";
+import ExpensesFilter from "./ExpenseFilter";
 import "../../css/Expenses.css";
 import Card from "../UI/Card";
 // import "../css/Card.css";
@@ -17,9 +19,20 @@ function RenderExpenses() {
     // console.log(ExpensesData);
   };
 
+  const [filterYear, setFilterYear] = useState("2020");
+  const filterByYearHandler = (selectedYear) => {
+    console.log("This is from RenderExpense.JS");
+    setFilterYear(selectedYear);
+    console.log(selectedYear.target.value);
+  };
+
   return (
     <Card className="expenses">
       <NewExpense addExpenseHere={generateNewExpense} />
+      <ExpensesFilter
+        defaultYear={filterYear}
+        onChangeYear={filterByYearHandler}
+      />
       {ExpensesData.map((insurance) => {
         return (
           <div key={insurance.id}>
